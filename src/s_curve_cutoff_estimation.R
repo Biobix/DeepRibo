c78f0c663587c019ebcc1c950e9a09fdd8b0563f
jdcla <jdcla@users.noreply.github.com>
2018-06-29 11:37:31 +0200

    improve model fitting by detecting cases in which the bent curve fits the upper bent of the four parameter s curve

--- src/s_curve_cutoff_estimation.R
+++ src/s_curve_cutoff_estimation.R
@@ -1,23 +1,45 @@
 library("splines")
 library("SiZer")
 get_cutoff_values <- function(path,dest){
+  print('Fitting data...')
+  # Loading data
   df <- read.csv(path)
   df<- df[df$label==1,c("rpk_elo","coverage_elo")]
+  # Transforming data
   mask <- df$rpk_elo != 0
   df <- df[mask,]
   df$rpk_elo <- log(df$rpk_elo)
+  rownames(df) <- NULL
+  # Fitting the S-curve
   formula <- coverage_elo ~ SSfpl(rpk_elo, D, A, C, B)
   model <- nls(formula, data=df)
-  bent_curve<- bent.cable(df$rpk_elo, predict(model, df$rpk_elo))
+  # Prepare data
+  df_fit = cbind(df[order(df$rpk_elo),])
+  fit_idx = length(df$rpk_elo)
+  decr_step = max(round(fit_idx/800),4)
+
+  while (MINCOV>0.60){
+    df_temp = cbind(df_fit[1:fit_idx,])
+    # Fit bent curve
+    bent_curve<- bent.cable(df_temp$rpk_elo, 
+                            predict(model, df_temp))
+    MINRPKM <- round(bent_curve$alpha, digits = 6)
+    MINCOV <- round(predict(bent_curve, MINRPKM), digits = 6)
+    # Drop upper right data points in order to force 
+    # the function to fit to the lower bent
+    if (MINCOV>0.60){
+      cat('Bent point fitted to upper bent...\nRefitting data...')
+    }
+    fit_idx <- fit_idx - decr_step
+  }
   
-  png(filename=paste(path,".png",sep=""))
-  plot(df$rpk_elo, df$coverage, xlab="RPKM",ylab="Coverage")
+  dest <- paste(dest,".png",sep="")
+  # Plot data
+  png(filename=dest)
+  plot(df$rpk_elo, df$coverage_elo, xlab="RPKM",ylab="Coverage")
   title("four parameter S curve estimation")
-  points(predict(model, df$rpk_elo)~ df$rpk_elo, col="green")
-  points( df$rpk_elo, predict(bent_curve, df$rpk_elo), col="red")
-  
-  MINRPKM <- round(bent_curve$alpha, digits = 2)
-  MINCOV <- round(predict(bent_curve, MINRPKM), digits = 2)
+  points(df$rpk_elo, predict(model, df), col="green")
+  points(df$rpk_elo, predict(bent_curve, df)[,1], col="red")  
   abline(h=MINCOV, col="blue")
   abline(v=MINRPKM, col="blue")
   dev.off()
